22115d3e1b8c1c12f0ae586077cc3686db20beba
Jim <easonjim@163.com>
2017-12-30 11:34:52 +0800

    Update templateengine.js

--- templateengine.js
+++ templateengine.js
@@ -16,8 +16,7 @@ function template_setdata(template, data) {
                 out += tempout;
             }
             return out;
-        }
-        else if(!!data && '[object Object]' == Object.prototype.toString.call(data)){//对象        
+        } else if(!!data && '[object Object]' == Object.prototype.toString.call(data)){//对象        
             for (var key in data) {
                 template = template.replace(/\{\{(\S+)\}\}/g,
                     function (m, i, j) {
@@ -25,8 +24,7 @@ function template_setdata(template, data) {
                     });
             }                        
             return template;
-        }
-        else{//不做处理
+        } else{//不做处理
             return template;                        
         }
     } catch (e) {
@@ -101,22 +99,22 @@ function template2_setdata(template, data) {
          <# 		=> ');
          #> 		=> p.push('
      * */
-    var result="var p=[];with(obj){ p.push('"//加入with设置作用域
+    var result="var p=[];with(data){ p.push('"//加入with设置作用域
     +html.replace(/[\r\n\t]/g," ")//换行去掉
     .replace(/<#=(.*?)#>/g,"');p.push($1);p.push('")//<#=xxx#> => ');p.push(xxx);p.push('
     .replace(/<#/g,"');")//<# => ');    
     .replace(/#>/g,"p.push('")//#> => p.push('
     +" ');}return p.join('');";//最后连接成一条字符串返回
-    var func = new Function('obj',result);//设置Function
-    return func(setdatas);
+    var func = new Function('data',result);//设置Function
+    return func(data);
 }
 /*
 使用方法：
 html页面模板：
 <script type="text/template" id="template2_info_content">
     <ul>
-        <# for ( var i = 0; i < setdatas.length; i++ ) { #>
-             <li><a href="<#=setdatas[i].url#>"><#=setdatas[i].title#></a></li>
+        <# for ( var i = 0; i < data.length; i++ ) { #>
+             <li><a href="<#=data[i].url#>"><#=data[i].title#></a></li>
         <# } #>
     </ul>
 </script>
